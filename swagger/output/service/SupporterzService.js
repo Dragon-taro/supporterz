1b7975afd9cf9803572578b48d460bc83bf5512e
RikiyaFujii <ridai22hanriku@gmail.com>
2019-04-27 13:15:35 +0900

    update swagger

--- swagger/output/service/SupporterzService.js
+++ swagger/output/service/SupporterzService.js
@@ -2,11 +2,11 @@
 
 
 /**
- * ユーザーの一覧を表示
+ * ユーザーの一覧
  *
  * returns SwUserListResponse
  **/
-exports.usersGET = function() {
+exports.v1UsersGET = function() {
   return new Promise(function(resolve, reject) {
     var examples = {};
     examples['application/json'] = {
@@ -19,6 +19,93 @@ exports.usersGET = function() {
     "id" : 0,
     "email" : "email"
   } ]
+};
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * ユーザーの削除
+ *
+ * id Long ユーザーID
+ * returns SwPostResponse
+ **/
+exports.v1UsersIdDELETE = function(id) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = {
+  "status" : "status"
+};
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * ユーザーの詳細
+ *
+ * id Long ユーザーID
+ * returns SwUserResponse
+ **/
+exports.v1UsersIdGET = function(id) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = {
+  "name" : "name",
+  "id" : 0,
+  "email" : "email"
+};
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * ユーザーの編集
+ *
+ * id Long ユーザーID
+ * body SwAddUser  (optional)
+ * returns SwPostResponse
+ **/
+exports.v1UsersIdPUT = function(id,body) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = {
+  "status" : "status"
+};
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * ユーザーの追加
+ *
+ * body SwAddUser  (optional)
+ * returns SwPostResponse
+ **/
+exports.v1UsersPOST = function(body) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = {
+  "status" : "status"
 };
     if (Object.keys(examples).length > 0) {
       resolve(examples[Object.keys(examples)[0]]);
